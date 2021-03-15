ac85eb2eb6d709c57143f46c6f9a3e24e5f1274e
SULY\mferhi <mferhi@suly.ca>
2021-03-15 16:15:07 -0400

    remove bin and obj

--- Autoveille/Scripts/table.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-$(document).ready(function () {
-$('#dtDynamicVerticalScrollExample').DataTable({
-"scrollY": "200px",
-"scrollCollapse": true,
-});
-$('.dataTables_length').addClass('bs-select');
-});
